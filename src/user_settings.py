54783fdac83cc8ae5435777c6a663b5fdc43d69e
sami4030 <sami.akherati+401@gmail.com>
2025-04-05 18:30:39 +0200

    Update user_settings.py

--- src/user_settings.py
+++ src/user_settings.py
@@ -3,23 +3,9 @@
 # List of source URLs to fetch proxy configurations from.
 # Add or remove URLs as needed. All URLs in this list are automatically enabled.
 SOURCE_URLS = [
-    "https://raw.githubusercontent.com/mahsanet/MahsaFreeConfig/refs/heads/main/mtn/sub_1.txt",
+    "https://t.me/s/WireVpnGuard",
     "https://t.me/s/v2rayfree",
     "https://t.me/s/FreeV2rays",
-    "https://t.me/s/v2ray_free_conf",
-    "https://t.me/s/PrivateVPNs",
-    "https://t.me/s/IP_CF_Config",
-    "https://t.me/s/shadowproxy66",
-    "https://t.me/s/OutlineReleasedKey",
-    "https://t.me/s/prrofile_purple",
-    "https://t.me/s/proxy_shadosocks",
-    "https://t.me/s/meli_proxyy",
-    "https://t.me/s/DirectVPN",
-    "https://t.me/s/VmessProtocol",
-    "https://t.me/s/ViProxys",
-    "https://t.me/s/heyatserver",
-    "https://t.me/s/vpnfail_vless",
-    "https://t.me/s/DailyV2RY",
     # Add more URLs here if you want to include additional sources.
 ]
 
@@ -34,7 +20,7 @@ SPECIFIC_CONFIG_COUNT = 100
 # Dictionary of protocols to enable or disable.
 # Set each protocol to True to enable, False to disable.
 ENABLED_PROTOCOLS = {
-    "wireguard://": False,
+    "wireguard://": True,
     "hysteria2://": True,
     "vless://": True,
     "vmess://": True,
