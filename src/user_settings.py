17af513be3dd725893e23dfc135e8ba251c47dde
sami4030 <sami.akherati+401@gmail.com>
2025-04-05 20:21:00 +0200

    Update user_settings.py

--- src/user_settings.py
+++ src/user_settings.py
@@ -4,7 +4,6 @@
 # Add or remove URLs as needed. All URLs in this list are automatically enabled.
 SOURCE_URLS = [
     "https://t.me/s/WireVpnGuard",
-    "https://t.me/WireVpnGuard",
     # Add more URLs here if you want to include additional sources.
 ]
 
