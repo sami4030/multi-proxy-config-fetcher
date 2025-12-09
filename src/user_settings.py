05549943d11d38700bd94449d41f827d5e751259
sami4030 <sami.akherati+401@gmail.com>
2025-12-09 22:21:25 +0330

    Update user_settings.py

--- src/user_settings.py
+++ src/user_settings.py
@@ -3,7 +3,7 @@
 # List of source URLs to fetch proxy configurations from.
 # Add or remove URLs as needed. All URLs in this list are automatically enabled.
 SOURCE_URLS = [
-    "https://t.me/s/WireVpnGuard",
+    "https://t.me/s/freeonlinevpn",
     # Add more URLs here if you want to include additional sources.
 ]
 
